ddcc33a63fcfe8cb9bde9392f9c60a03effc0d3e
Nirvana <xingf.tech@gmail.com>
2016-04-17 15:10:05 -0400

    update

--- routes/users.js
+++ routes/users.js
@@ -14,17 +14,26 @@ router.get('/', function(req, res, next) {
     })
     .catch(function(err) {
       console.log(err);
-      res.render('404', {err: err});
+      res.render('error', {err: err});
     });
 });
 
 router.post('/', function(req, res) {
+  var query = {};
   var data = req.body;
-  var name = data['name'];
-  console.log(name);
-  
+
+  var name = data['name'] || "";
+  var agefrom = data['agefrom'] || 0;
+  var ageto = data['ageto'] || 100;
+  var city = data['city'].split(',') || "";
+  /*var times = data['times'] || "";*/
+
+  query['name'] = new RegExp(name);
+  query['age'] = {$gt: agefrom, $lt: ageto};
+  query['favourites.cities'] = {$in: city}
+
   Users
-      .find({'name' : name})
+      .find(query)
       .exec()
       .then(function(users) {
         // users.name = 
@@ -32,9 +41,10 @@ router.post('/', function(req, res) {
       })
       .catch(function(err) {
       console.log(err);
-      res.render('404', {err: err});
+      res.render('error', {err: err});
     });
 });
+
 
 
 module.exports = router;
