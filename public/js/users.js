9097e78b2e8c32298bc395e549928fe24415b0c3
Nirvana <xingf.tech@gmail.com>
2016-04-18 04:15:46 -0400

    update

--- public/js/users.js
+++ public/js/users.js
@@ -1,3 +1,4 @@
+localStorage.setItem('order', "dsec");
 $("#sub").on("click", function(e){
   e.preventDefault();
   var senddata = $("#form").serialize();  
@@ -10,8 +11,7 @@ $("#sub").on("click", function(e){
     success: function(data, textStatus, jqXHR){
       localStorage.setItem('currentdata', JSON.stringify(data));
       console.log(localStorage.getItem('currentdata'));
-      //$("#usertable > tbody").empty();
-      $("#tbody").children().remove()
+      $("#usertable > tbody").empty();
       var resdata = "";
 
       for(var i = 0; i< data.length; i++){
@@ -22,7 +22,7 @@ $("#sub").on("click", function(e){
         resdata+= data[i]["favourites"]["times"] + "</td>";
         resdata+= "</tr>";
       }
-      $("#usertbody").after(resdata);
+      $("#usertbody").append(resdata);
     }
   });  
 }); 
@@ -32,20 +32,35 @@ $('#namesort').on("click", function(e){
 
   var data = JSON.parse(localStorage.getItem('currentdata'));
   console.log(data);
-  data.sort(function(a,b){
-    if (a.name > b.name){
-      return 1;
-    }
-    if (a.name < b.name){
-      return -1;
-    }
-    return 0;
-  });
+  if (localStorage.getItem('order') == "dsec"){
+    localStorage.setItem('order', "asec");
+    data.sort(function(a,b){
+      if (a.name > b.name){
+        return 1;
+      }
+      if (a.name < b.name){
+        return -1;
+      }
+      return 0;
+    });
+  }
 
-  var retdata = JSON.parse(data);
+  else{
+    localStorage.setItem('order', "dsec");
+    data.sort(function(a,b){
+      if (a.name < b.name){
+        return 1;
+      }
+      if (a.name > b.name){
+        return -1;
+      }
+      return 0;
+    });
+  }
+  
 
   $("#usertable > tbody").empty();
-
+  var resdata = "";
   for(var i = 0; i< data.length; i++){
     resdata+= "<tr>";
     resdata+= "<td>" + data[i]["name"] + "</td>";
@@ -54,16 +69,52 @@ $('#namesort').on("click", function(e){
     resdata+= data[i]["favourites"]["times"] + "</td>";
     resdata+= "</tr>";
   }
-  $("#usertbody").after(resdata);
+  $("#usertbody").append(resdata);
 }); 
 
 $('#agesort').on("click", function(e){
   e.preventDefault();
 
-  var data = localStorage.getItem("currentdata");
+  var data = JSON.parse(localStorage.getItem('currentdata'));
   console.log(data);
+  if (localStorage.getItem('order') == "dsec"){
+    localStorage.setItem('order', "asec");
+    data.sort(function(a,b){
+      if (a.age > b.age){
+        return 1;
+      }
+      if (a.age < b.age){
+        return -1;
+      }
+      return 0;
+    });
+  }
+
+  else{
+    localStorage.setItem('order', "dsec");
+    data.sort(function(a,b){
+      if (a.age < b.age){
+        return 1;
+      }
+      if (a.age > b.age){
+        return -1;
+      }
+      return 0;
+    });
+  }
+  
+
+  $("#usertable > tbody").empty();
+  var resdata = "";
+  for(var i = 0; i< data.length; i++){
+    resdata+= "<tr>";
+    resdata+= "<td>" + data[i]["name"] + "</td>";
+    resdata+= "<td>" + data[i]["age"] + "</td>";
+    resdata+= "<td>" + data[i]["favourites"]["cities"] + "<br>";
+    resdata+= data[i]["favourites"]["times"] + "</td>";
+    resdata+= "</tr>";
+  }
+  $("#usertbody").append(resdata);
 }); 
 
-function compare(a, b){
-  return a - b;
-}
+$('.ui-timepicker-input').timepicker();
